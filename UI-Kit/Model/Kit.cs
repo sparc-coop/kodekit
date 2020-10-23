8662f0245de578e721db95bfa26ba9460243ee07
Margaret Landefeld <mlandefeld@gmail.com>
2020-10-23 14:32:33 -0400

    Added styling and pages for css creation flow

--- UI-Kit/Model/Kit.cs
+++ UI-Kit/Model/Kit.cs
@@ -1,37 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace UI_Kit.Model
 {
     public class Kit
     {
-        [Required]
-        [StringLength(300, ErrorMessage = "Name is too long.")]
-        public string Name { get; set; }
-        //public string Input { get; set; }
-        //public string Textarea { get; set; }
-        //public string H1 { get; set; }
-        //public string H2 { get; set; }
+
+        public Kit()
+        {
+            //Kit = new HashSet<Kit>();
+        }
 
         [Key]
-        public int id { get; set; }
-        public int UserId { get; set; }
-        public Guid GUID { get; set; }
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public Guid? GUID { get; set; }
         public string Description { get; set; }
-        //public string FileName { get; set; }
         public string Url { get; set; }
-        //public string GUID { get; set; }
-
-        //public Kit()
-        //{
-
-        //}
-
-    //    public Kit(int userId, string name, string url)
-    //    {
-    //        //KitGUID = Guid.NewGuid();
-    //        UserId = userId;
-    //        Name = name;
-    //    }
+        //[Required]
+        //[StringLength(300, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public string TertiaryColor { get; set; }
+        public string DarkColor { get; set; }
+        public string LightColor { get; set; }
+        public string TypeScale { get; set; }
+        public string HeaderFont { get; set; }
+        public string HeaderFontSize { get; set; }
+        public string HeaderTypeScale { get; set; }
+        public string HeaderLineHeight { get; set; }
+        public string BodyFont { get; set; }
+        public string BodyFontSize { get; set; }
+        public string BodyTypeScale { get; set; }
+        public string BodyLineHeight { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public bool? IsFinished { get; set; }
     }
 }
