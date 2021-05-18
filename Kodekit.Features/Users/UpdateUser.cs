58dbef5346f7abe56930d9e097d63bae8162dc7a
Margaret Landefeld <mlandefeld@gmail.com>
2021-05-18 16:41:38 -0400

    Initial rearchitecture with sparc

---
+++ Kodekit.Features/Users/UpdateUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sparc.Features;
+using Sparc.Core;
+using Kodekit.Core;
+
+namespace Kodekit.Features
+{
+    public class UpdateUser : Feature<User, bool>
+    {
+        public UpdateUser(IRepository<User> users)
+        {
+            Users = users;
+        }
+
+        public IRepository<User> Users { get; }
+
+        public override async Task<bool> ExecuteAsync(User user)
+        {
+            try
+            {
+                await Users.UpdateAsync(user);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
