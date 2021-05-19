2c3582afe589f706e569ebbcc7325310d6567e83
Margaret Landefeld <mlandefeld@gmail.com>
2021-05-19 17:52:20 -0400

    Working ad b2c authentication

--- Kodekit.Features/Startup.cs
+++ Kodekit.Features/Startup.cs
@@ -39,24 +39,6 @@ namespace Kodekit.Features
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kodekit.Features v1"));
-            //}
-
-            //app.UseHttpsRedirection();
-
-            //app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllers();
-            //});
-
             app.Sparcify<Startup>(env);
         }
     }
