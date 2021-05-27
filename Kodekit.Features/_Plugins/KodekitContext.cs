83e6983bdb4a4306de0ac09ea890067b769a08a3
Margaret Retzinger <maretzinger@gmail.com>
2021-05-27 10:22:55 -0400

    Progress for cosmosdb setup

--- Kodekit.Features/_Plugins/KodekitContext.cs
+++ Kodekit.Features/_Plugins/KodekitContext.cs
@@ -8,18 +8,26 @@ namespace Kodekit.Features
         public KodekitContext(DbContextOptions<KodekitContext> options) : 
             base(options) { }
 
-        public virtual DbSet<User> User { get; set; }
-        public virtual DbSet<Kit> Kit { get; set; }
+        //public virtual DbSet<User> User { get; set; }
+        //public virtual DbSet<Kit> Kit { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    builder.Entity<User>(builder => {
+        //        builder.HasKey(x => x.Id);
+        //    });
+
+        //    builder.Entity<Kit>(builder => {
+        //        builder.HasKey(x => x.Id);
+        //    });
+        //}
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            modelBuilder.Entity<User>(builder => {
-                builder.HasKey(x => x.Id);
-            });
+            var user = builder.Entity<User>().HasPartitionKey(x => x.UserId);
+            //user.OwnsMany(x => x.Kits);
 
-            modelBuilder.Entity<Kit>(builder => {
-                builder.HasKey(x => x.Id);
-            });
+            builder.Entity<Kit>().HasPartitionKey(x => x.UserId);
         }
 
     }
