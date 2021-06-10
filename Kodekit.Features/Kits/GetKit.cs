685865d78a7314bbb5d5937cfcd036433e675fa6
Margaret Retzinger <maretzinger@gmail.com>
2021-06-10 18:08:05 -0400

    Saving user id to a kit and basic styling to additional pages

---
+++ Kodekit.Features/Kits/GetKit.cs
@@ -0,0 +1,21 @@
+﻿using Kodekit.Core;
+using Sparc.Core;
+using Sparc.Features;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Kodekit.Features
+{
+    public class GetKit : Feature<string, Kit>
+    {
+        public GetKit(IRepository<Kit> kits)
+        {
+            Kits = kits;
+        }
+
+        public IRepository<Kit> Kits { get; }
+
+        public override async Task<Kit> ExecuteAsync(string kitId) => await Kits.FindAsync(kitId); 
+    }
+}
