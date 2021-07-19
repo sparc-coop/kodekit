e2aef4bca165930d38878642ab31da8d772f6ddd
Layla Hedges <layla.hedges@gmail.com>
2021-07-19 17:08:06 -0400

    Updated contrast checker for multiple values

--- Kodekit.Features/_Plugins/ColorContrastChecker.cs
+++ Kodekit.Features/_Plugins/ColorContrastChecker.cs
@@ -8,10 +8,11 @@ using System.Net.Http.Json;
 using static System.Net.WebRequestMethods;
 using Newtonsoft.Json;
 using Sparc.Features;
+using System.Reflection;
 
 namespace Kodekit.Features
 {
-    public class ColorContrastChecker : PublicFeature<Kit, ContrastColorResponse>
+    public class ColorContrastChecker : PublicFeature<Kit, List<ContrastColorResponse>>
     {
         string baseurl = "https://webaim.org/resources/contrastchecker/?";
         string clrs = "";
@@ -19,35 +20,62 @@ namespace Kodekit.Features
         {
             //clrs = colors;
         }
-        public override async Task<ContrastColorResponse> ExecuteAsync(Kit kit)
+        public override async Task<List<ContrastColorResponse>> ExecuteAsync(Kit kit)
         {
             HttpClient client = new HttpClient();
             //fcolor=0000FF&bcolor=FFFFFF&api
 
-            var apiurl = baseurl + "fcolor=" + kit.PrimaryColor.Substring(1) + "&bcolor=" + kit.SecondaryColor.Substring(1) + "&api";
-            var url = new Uri(apiurl);
+
             //var apiurl = baseurl + colors + "&api";
 
+            //var apiurl = baseurl + "fcolor=" + kit.BackgroundColor.Substring(1) + "&bcolor=" + kit.DarkColor.Substring(1) + "&api";
+
+            //backgroud + primary, secondary, tertiary, darkest, lightest?
+            //primary + darkest, lightest?
+
             ContrastColorResponse colorContrast = new ContrastColorResponse();
+            List<ContrastColorResponse> resultsList = new List<ContrastColorResponse>();
 
             try
             {
-                var response = await client.GetAsync(url);
-                //string checkResult = response.ToString();
-                var responseBody = await response.Content.ReadAsStringAsync();
-                //var answer = JsonConvert.DeserializeObject<string>(checkResult);
-
-                var myParsedObject = JsonConvert.DeserializeObject<ContrastColorResponse>(responseBody); 
                 
-                //(ContrastColorResponse)(new JsonConvert).Deserialize(contentString, typeof(MyObject));
+                //Record record = new Record();
+                string colorCompare = "";
+                string testing = "";
+                ContrastColorResponse contrastResults = new ContrastColorResponse();
+                PropertyInfo[] properties = typeof(Kit).GetProperties();
+                foreach (PropertyInfo property in properties.Skip(6).Take(5))
+                {
+
+                    colorCompare = property.GetValue(kit).ToString();
+                    testing = property.Name;
+
+
+                    var apiurl = baseurl + "fcolor=" + kit.PrimaryColor.Substring(1) + "&bcolor=" + colorCompare.Substring(1) + "&api";
+                    var url = new Uri(apiurl);
+                    var response = await client.GetAsync(url);
+                    var responseBody = await response.Content.ReadAsStringAsync();
+
+                    contrastResults = JsonConvert.DeserializeObject<ContrastColorResponse>(responseBody);
+                    //contrastResults.Failed = new List<string>();
+                    contrastResults.ColorA = "PrimaryColor";
+                    contrastResults.ColorB = property.Name;
+
+                    if (contrastResults.AA == "fail")
+                    {
+                        resultsList.Add(contrastResults);
+                    }
+
+                    
+                }
                 client.Dispose();
-                return myParsedObject;
+                return resultsList;
             }
             catch (Exception ex)
             {
                 string checkResult = "Error " + ex.ToString();
                 client.Dispose();
-                return colorContrast;
+                return resultsList;
                 //return checkResult;
             }
 
@@ -80,6 +108,9 @@ namespace Kodekit.Features
         public string AALarge { get; set; }
         public string AAALarge { get; set; }
         public string AAA { get; set; }
+        public List<string> Failed {get; set;}
+        public string ColorA { get; set; }
+        public string ColorB { get; set; }
     }
 
 }
