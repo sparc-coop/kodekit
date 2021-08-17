51975e4930b34897ef17db7102d111bef32f2a62
Margaret Retzinger <maretzinger@gmail.com>
2021-08-17 14:56:22 -0400

    Minor fixes

--- Kodekit.Features/Kits/GetRelatedKits.cs
+++ Kodekit.Features/Kits/GetRelatedKits.cs
@@ -17,7 +17,9 @@ namespace Kodekit.Features
 
         public override async Task<List<Kit>> ExecuteAsync(string kitId)
         {
-            return await Kits.Query.Where(x => x.UserId == User.Id() && (x.ParentId == kitId || x.Id == kitId))// && x.IsDeleted != true)
+            return await Kits.Query.Where(x => x.UserId == User.Id() 
+                && (x.ParentId == kitId || x.KitId == kitId) 
+                && x.IsDeleted != true)
                 .OrderByDescending(x => x.DateCreated)
                 .ToListAsync();
         }
