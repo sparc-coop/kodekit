7814d7614abe9e0fe9da7167f20953f09aa65edd
Joseph Young <joseph@josephayoung.com>
2021-09-08 13:33:05 -0500

    Effects rearch

---
+++ Kodekit.Features/Elements/Effects/UpdateShadow.cs
@@ -0,0 +1,31 @@
+﻿using Sparc.Core;
+using Sparc.Features;
+using System.Threading.Tasks;
+
+namespace Kodekit.Features.Elements
+{
+    public record ShadowsModel(string KitId, ShadowModel Small, ShadowModel XLarge);
+    public record ShadowModel(double X, double Y, double Blur, double Spread, string Color, double Opacity);
+    public class UpdateShadow : Feature<ShadowsModel, Kit>
+    {
+        public UpdateShadow(IRepository<Kit> kits)
+        {
+            Kits = kits;
+        }
+
+        public IRepository<Kit> Kits { get; }
+
+        public override async Task<Kit> ExecuteAsync(ShadowsModel request)
+        {
+            var kit = await Kits.FindAsync(request.KitId);
+
+            var smallShadow = new Shadow(request.Small.X, request.Small.Y, request.Small.Blur, request.Small.Spread, request.Small.Color, request.Small.Opacity);
+            var xLargeShadow = new Shadow(request.XLarge.X, request.XLarge.Y, request.XLarge.Blur, request.XLarge.Spread, request.XLarge.Color, request.XLarge.Opacity);
+
+            kit.UpdateShadows(smallShadow, xLargeShadow);
+
+            await Kits.UpdateAsync(kit);
+            return kit;
+        }
+    }
+}
---
+++ Kodekit.Features/Kits/Entities/Shadow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kodekit.Features.Elements
+{
+    public class Shadow
+    {
+        public Shadow(double x, double y, double blur, double spread, string color, double opacity)
+        {
+            X = x;
+            Y = y;
+            Blur = blur;
+            Spread = spread;
+            Color = color;
+            Opacity = opacity;
+        }
+
+
+        public double X {  get; set; }
+        public double Y {  get; set; }
+        public double Blur {  get; set; }
+        public double Spread {  get; set; }
+        public string Color {  get; set; }
+        public double Opacity {  get; set; }
+
+        public Dictionary<string, Shadow> GenerateWeights(Shadow xLargeShadow)
+        {
+            return new Dictionary<string, Shadow>
+            {
+                { "small", this },
+                { "medium", Interpolate(xLargeShadow, 0.33) },
+                { "large", Interpolate(xLargeShadow, 0.66) },
+                { "xlarge", xLargeShadow }
+            };
+        }
+
+        private Shadow Interpolate(Shadow other, double percentage)
+        {
+            Func<double, double, double> between = (double min, double max) => min + ((max - min) * percentage);
+            
+            return new Shadow(between(X, other.X), 
+                between(Y, other.Y),
+                between(Blur, other.Blur),
+                between(Spread, other.Spread),
+                new Color(Color).Interpolate(new Color(other.Color), (float)percentage).HexValue,
+                between(Opacity, other.Opacity));
+        }
+    }
+}
