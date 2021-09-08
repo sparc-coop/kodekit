3bc26738becfd94a6a5ba4eb1b140f6100e611ca
Joseph Young <joseph@josephayoung.com>
2021-09-08 15:57:18 -0500

    Button arch

---
+++ Kodekit.Features/Elements/Buttons/Button.cs
@@ -0,0 +1,40 @@
+﻿namespace Kodekit.Features.Elements
+{
+    public class Button : Element
+    {
+        public Button() : base("button")
+        {
+        }
+
+        public Button(double fontSize, int fontWeight, double verticalPadding, double horizontalPadding, double cornerRadius,
+            double borderWidth, double iconWidth, double iconHeight, bool removeSecondaryBorder) : this()
+        {
+            FontSize = new(fontSize);
+            FontWeight = new(fontWeight);
+            VerticalPadding = new(verticalPadding);
+            HorizontalPadding = new(horizontalPadding);
+            CornerRadius = new(cornerRadius);
+            BorderWidth = new(borderWidth);
+            IconWidth = new(iconWidth);
+            IconHeight = new(iconHeight);
+            RemoveSecondaryBorder = removeSecondaryBorder;
+        }
+
+        [Css("font-size")]
+        public Size FontSize { get; set; }
+        [Css("font-weight")]
+        public Weight FontWeight { get; set; }
+        [Css("padding-top", "padding-bottom")]
+        public Size VerticalPadding { get; set; }
+        [Css("padding-left", "padding-right")]
+        public Size HorizontalPadding { get; set; }
+        [Css("border-radius")]
+        public Size CornerRadius { get; set; }
+        [Css("border-width")]
+        public Size BorderWidth { get; set; }
+        public Size IconWidth { get; set; }
+        public Size IconHeight { get; set; }
+        public bool RemoveSecondaryBorder { get; set; }
+
+    }
+}
---
+++ Kodekit.Features/Elements/Buttons/UpdateButtons.cs
@@ -0,0 +1,29 @@
+﻿using Sparc.Core;
+using Sparc.Features;
+using System.Threading.Tasks;
+
+namespace Kodekit.Features.Elements
+{
+    public record UpdateButtonsModel(string KitId, double FontSize, int FontWeight, double VerticalPadding, double HorizontalPadding, double CornerRadius, double BorderWidth, double IconWidth, double IconHeight, bool RemoveSecondaryBorder);
+    public class UpdateButtons : Feature<UpdateButtonsModel, Kit>
+    {
+        public UpdateButtons(IRepository<Kit> kits)
+        {
+            Kits = kits;
+        }
+
+        public IRepository<Kit> Kits { get; }
+
+        public override async Task<Kit> ExecuteAsync(UpdateButtonsModel request)
+        {
+            var kit = await Kits.FindAsync(request.KitId);
+
+            var buttons = new Button(request.FontSize, request.FontWeight, request.VerticalPadding, request.HorizontalPadding, request.CornerRadius, request.BorderWidth, request.IconWidth, request.IconHeight, request.RemoveSecondaryBorder);
+
+            kit.UpdateButtons(buttons);
+            await Kits.UpdateAsync(kit);
+
+            return kit;
+        }
+    }
+}
