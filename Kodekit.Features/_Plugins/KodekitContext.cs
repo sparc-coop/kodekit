cb9eca7e84bb934d554af727539885278b0aa395
Joseph Young <joseph@josephayoung.com>
2021-09-12 11:41:33 -0500

    Model fixes

--- Kodekit.Features/_Plugins/KodekitContext.cs
+++ Kodekit.Features/_Plugins/KodekitContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kodekit.Features.Elements;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Kodekit.Features
 {
@@ -20,33 +22,58 @@ namespace Kodekit.Features
             shadows.OwnsOne(x => x.Value);
 
             var headings = kit.OwnsOne(x => x.Headings);
-            headings.OwnsOne(x => x.Font);
+            headings.OwnsOne(x => x.Font).Configure();
 
             var paragraphs = kit.OwnsOne(x => x.Paragraphs);
-            paragraphs.OwnsOne(x => x.Font);
+            paragraphs.OwnsOne(x => x.Font).Configure();
 
             var buttons = kit.OwnsOne(x => x.Buttons);
-            buttons.OwnsOne(x => x.Font);
-            buttons.OwnsOne(x => x.Border);
-            buttons.OwnsOne(x => x.Padding);
+            buttons.OwnsOne(x => x.Font).Configure();
+            buttons.OwnsOne(x => x.Border).Configure();
+
+            buttons.OwnsOne(x => x.Padding).Configure();
             buttons.OwnsOne(x => x.IconHeight);
             buttons.OwnsOne(x => x.IconWidth);
 
             var inputs = kit.OwnsOne(x => x.Inputs);
-            inputs.OwnsOne(x => x.Font);
-            inputs.OwnsOne(x => x.Border);
-            inputs.OwnsOne(x => x.Padding);
+            inputs.OwnsOne(x => x.Font).Configure();
+            inputs.OwnsOne(x => x.Border).Configure();
+            inputs.OwnsOne(x => x.Padding).Configure();
 
             var selectors = kit.OwnsOne(x => x.Selectors);
-            selectors.OwnsOne(x => x.Font);
+            selectors.OwnsOne(x => x.Font).Configure();
             selectors.OwnsOne(x => x.ActiveColor);
 
             var settings = kit.OwnsOne(x => x.Settings);
 
             var dropdowns = kit.OwnsOne(x => x.Dropdowns);
-            dropdowns.OwnsOne(x => x.Font);
-            dropdowns.OwnsOne(x => x.Border);
-            dropdowns.OwnsOne(x => x.Padding);
+            dropdowns.OwnsOne(x => x.Font).Configure();
+            dropdowns.OwnsOne(x => x.Border).Configure();
+            dropdowns.OwnsOne(x => x.Padding).Configure();
+        }
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        // Won't need any of this once we go to .NET 6, because EF 6 has implicit ownership
+        // https://devblogs.microsoft.com/dotnet/taking-the-ef-core-azure-cosmos-db-provider-for-a-test-drive/
+        public static void Configure<T>(this OwnedNavigationBuilder<T, Border> border) where T : class
+        {
+            border.OwnsOne(x => x.Color);
+            border.OwnsOne(x => x.Radius);
+            border.OwnsOne(x => x.Width);
+        }
+
+        public static void Configure<T>(this OwnedNavigationBuilder<T, Font> font) where T : class
+        {
+            font.OwnsOne(x => x.Size);
+            font.OwnsOne(x => x.LineHeight);
+        }
+
+        public static void Configure<T>(this OwnedNavigationBuilder<T, Padding> padding) where T : class
+        {
+            padding.OwnsOne(x => x.Vertical);
+            padding.OwnsOne(x => x.Horizontal);
         }
     }
 }
