6b99afb65f5fc0107a14cc9e9edb34fb59f98781
Joseph Young <joseph@josephayoung.com>
2021-09-13 09:29:58 -0500

    Css generation fixes

--- Kodekit.Features/Elements/_Shared/ISerializable.cs
+++ Kodekit.Features/Elements/_Shared/ISerializable.cs
@@ -12,7 +12,7 @@ namespace Kodekit.Features.Elements
     {
         public static Dictionary<string, string> Concat(this ISerializable item, ISerializable item2)
         {
-            return item.Concat(item2).ToDictionary(x => x.Key, x => x.Value);
+            return item.Serialize().Concat(item2).ToDictionary(x => x.Key, x => x.Value);
         }
 
         public static Dictionary<string, string> Concat(this Dictionary<string, string> item, ISerializable item2)
