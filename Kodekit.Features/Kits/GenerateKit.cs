c4b3b1e2bc99e54b5bcc8fcd5295f88d5644ae3c
Joseph Young <joseph@josephayoung.com>
2021-09-13 16:01:25 -0500

    Additional fixes

--- Kodekit.Features/Kits/GenerateKit.cs
+++ Kodekit.Features/Kits/GenerateKit.cs
@@ -25,8 +25,11 @@ namespace Kodekit.Features
         public async Task<IActionResult> HandleAsync(string kitId, bool? preview)
         {
             var kit = await Kits.FindAsync(kitId);
+            // Todo: lookup the kit that is actually published
+            
             if (kit == null)
                 throw new Exception("Kit not found!");
+
 
             var css = new StringBuilder();
             css.AppendLine(GetLocalFile("Elements/_Shared/destyle-reset.css"));
