18d168f1c816490846cfe07f3510f0a851c91e3c
Joseph Young <joseph@josephayoung.com>
2021-09-13 09:03:35 -0500

    Tweaks to new kit flow and dashboard

---
+++ Kodekit.Features/Kits/CopyKit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sparc.Core;
+using Sparc.Features;
+
+namespace Kodekit.Features
+{
+    public class CopyKit : PublicFeature<string, CreateKitResponse>
+    {
+        public IRepository<Kit> Kits { get; }
+        public CopyKit(IRepository<Kit> kit) => Kits = kit;
+
+        public override async Task<CreateKitResponse> ExecuteAsync(string kitId)
+        {
+            var kit = await Kits.FindAsync(kitId);
+            if (kit == null)
+                throw new NotFoundException("Couldn't find kit!");
+
+            kit.Id = Guid.NewGuid().ToString();
+            kit.UserId = User.Id();
+            kit.DateCreated = DateTime.Now;
+            kit.ParentId = kitId;
+
+            await Kits.AddAsync(kit);
+            
+            return new(kit.Id);
+        }
+    }
+}
