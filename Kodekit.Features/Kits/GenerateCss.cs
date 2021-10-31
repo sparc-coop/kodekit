e89ad6cacf270d17cb821947866674882dd28d5f
Joseph Young <joseph@josephayoung.com>
2021-10-31 11:29:28 -0500

    Radio styling

--- Kodekit.Features/Kits/GenerateCss.cs
+++ Kodekit.Features/Kits/GenerateCss.cs
@@ -79,7 +79,7 @@ namespace Kodekit.Features
             Compile(variables, scope, kit.Paragraphs);
             Compile(variables, "h1, h2, h3, h4, h5, h6, .subtitle", kit.Headings);
             Compile(variables, "button", kit.Buttons);
-            Compile(variables, "input[type=checkbox]", kit.Selectors);
+            Compile(variables, "input[type=checkbox], input[type=radio]", kit.Selectors);
 
             Compile(variables, "input, label, textarea" + (!kit.Dropdowns.OverwriteInherited ? ", select" : ""), kit.Inputs);
             if (kit.Dropdowns.OverwriteInherited)
