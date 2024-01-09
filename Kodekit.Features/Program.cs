fe7601fea1b2b89457178049b2ec1c41758a8939
Joseph Young <joseph@josephayoung.com>
2024-01-09 13:29:37 -0600

    Update to new Program.cs pattern

--- Kodekit.Features/Program.cs
+++ Kodekit.Features/Program.cs
@@ -1,5 +1,8 @@
 //using Kodekit.Web.Pages;
+using Kodekit.Features;
 using Kodekit.Features.Components;
+using Sparc.Authentication.AzureADB2C;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,21 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.Sparcify<App>();
+builder.Services.AddCosmos<KodekitContext>(builder.Configuration["ConnectionStrings:CosmosDb"]!, "kodekit");
+builder.Services.AddAzureADB2CAuthentication(builder.Configuration);
+builder.Services.AddScoped<KitRepository>();
+
 var app = builder.Build();
+
+app.UseCors(policy =>
+            policy.WithOrigins("https://localhost:5000", "https://localhost:5001",
+                "https://kodekit-test.azurewebsites.net",
+                "https://app.kodekit.io")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+app.Sparcify<App>(app.Environment);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
