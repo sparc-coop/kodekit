b2d4fad2b7d0dc21f443ea268cb003626a700161
Christine Antonio <christinejm.antonio@gmail.com>
2024-02-26 10:50:01 -0800

    update Program.cs

--- Kodekit/Program.cs
+++ Kodekit/Program.cs
@@ -13,11 +13,13 @@ builder.AddBlossom("https://localhost:5001");
 builder.Services.AddCosmos<KodekitContext>(builder.Configuration["ConnectionStrings:CosmosDb"]!, "kodekit", ServiceLifetime.Scoped);
 builder.Services.AddAzureADB2CAuthentication<User>(builder.Configuration);
 builder.Services.AddScoped<KitRepository>();
+builder.Services.AddScoped<ElementRepository>();
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddOutputCache();
 
 var app = builder.Build();
 app.UseBlossom();
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
