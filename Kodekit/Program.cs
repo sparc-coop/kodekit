1742d4ad436fe116643451d6793e371ec56694be
Christine Antonio <christinejm.antonio@gmail.com>
2024-02-28 09:45:57 -0800

    replace localstorage with persistentstate, comment out Authentication/AuthorizeView

--- Kodekit/Program.cs
+++ Kodekit/Program.cs
@@ -1,5 +1,7 @@
 using Kodekit;
 using Kodekit.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using Sparc.Blossom;
 using Sparc.Blossom.Authentication;
 using System.Configuration;
@@ -10,16 +12,31 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.AddBlossom("https://localhost:5001");
-builder.Services.AddCosmos<KodekitContext>(builder.Configuration["ConnectionStrings:CosmosDb"]!, "kodekit", ServiceLifetime.Scoped);
-builder.Services.AddAzureADB2CAuthentication<User>(builder.Configuration);
+//builder.Services.AddCosmos<KodekitContext>(builder.Configuration["ConnectionStrings:CosmosDb"]!, "kodekit", ServiceLifetime.Scoped);
+
+// Debugging AddCosmos
+string connectionString2 = builder.Configuration["ConnectionStrings:CosmosDb"]!;
+string databaseName2 = "kodekit";
+builder.Services.AddDbContext<KodekitContext>(delegate (DbContextOptionsBuilder options)
+{
+    options.UseCosmos(connectionString2, databaseName2, delegate (CosmosDbContextOptionsBuilder options)
+    {
+    });
+}, ServiceLifetime.Scoped);
+builder.Services.Add(new ServiceDescriptor(typeof(DbContext), typeof(KodekitContext), ServiceLifetime.Scoped));
+builder.Services.AddTransient((IServiceProvider sp) => new CosmosDbDatabaseProvider(sp.GetRequiredService<DbContext>(), databaseName2));
+//builder.Services.Add(new ServiceDescriptor(typeof(IRepository<>), typeof(CosmosDbRepository<>), ServiceLifetime.Scoped));builder.Services.AddAzureADB2CAuthentication<User>(builder.Configuration);
+
 builder.Services.AddScoped<KitRepository>();
 builder.Services.AddScoped<ElementRepository>();
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddOutputCache();
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
-app.UseBlossom();
+//app.UseBlossom();
 app.UseAntiforgery();
+app.UseAuthentication();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
