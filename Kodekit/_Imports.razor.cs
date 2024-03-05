44ebbc9413cc4735784567572af419c41eb42368
Joseph Young <joseph@josephayoung.com>
2024-03-05 09:13:37 -0600

    SCSS screaming architecture round 1

--- Kodekit/_Imports.razor.cs
+++ Kodekit/_Imports.razor.cs
@@ -1,37 +1,30 @@
-﻿global using System;
+﻿// .NET
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
-global using static System.Runtime.InteropServices.JavaScript.JSType;
-
-﻿global using System.Net.Http;
+global using System.Net.Http;
 global using System.Net.Http.Json;
+global using System.Net.NetworkInformation;
+
+// ASP.NET
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components.Routing;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.Web.Virtualization;
-global using static Microsoft.AspNetCore.Components.Web.RenderMode;
 global using Microsoft.JSInterop;
 
+// Blossom
 global using Sparc.Blossom;
 global using Sparc.Blossom.Data;
 global using Sparc.Blossom.Authentication;
-global using System.Net.NetworkInformation;
 
+// Kodekit
 global using Kodekit;
-global using Kodekit.Components;
-global using Kodekit.Components.Colors;
-global using Kodekit.Components.Icons;
-global using Kodekit.Components.Modals;
-global using Kodekit.Layouts;
-global using Kodekit.Layouts.Components;
-global using Kodekit.Models;
-global using Kodekit.Models.Elements;
-global using Kodekit.Models.Kits;
-global using Kodekit.Pages;
 global using static Kodekit.KitRepository;
 global using static Kodekit.UserRepository;
 
+// Plugins
 global using Blazored.LocalStorage;
 global using Blazor.Analytics;
