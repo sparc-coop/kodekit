4a68d53e53c0ff5419c8631724f36bd952e7625c
Joseph Young <joseph@josephayoung.com>
2024-03-11 11:33:41 -0500

    Start migrating to .NET 8 pattern with passwordless auth

--- Kodekit/Program.cs
+++ Kodekit/Program.cs
@@ -5,41 +5,15 @@ using Microsoft.EntityFrameworkCore;
 using Sparc.Blossom;
 using Sparc.Blossom.Authentication;
 using System.Configuration;
+using Sparc.Blossom.Authentication.Passwordless;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.AddBlossom("https://localhost:5001");
-//builder.Services.AddCosmos<KodekitContext>(builder.Configuration["ConnectionStrings:CosmosDb"]!, "kodekit", ServiceLifetime.Scoped);
-
-// Debugging AddCosmos
-string connectionString2 = builder.Configuration["ConnectionStrings:CosmosDb"]!;
-string databaseName2 = "kodekit";
-builder.Services.AddDbContext<KodekitContext>(delegate (DbContextOptionsBuilder options)
+var app = BlossomApplication.Run<App, User>(args,
+    (services, config) =>
 {
-    options.UseCosmos(connectionString2, databaseName2, delegate (CosmosDbContextOptionsBuilder options)
-    {
-    });
-}, ServiceLifetime.Scoped);
-builder.Services.Add(new ServiceDescriptor(typeof(DbContext), typeof(KodekitContext), ServiceLifetime.Scoped));
-builder.Services.AddTransient((IServiceProvider sp) => new CosmosDbDatabaseProvider(sp.GetRequiredService<DbContext>(), databaseName2));
-//builder.Services.Add(new ServiceDescriptor(typeof(IRepository<>), typeof(CosmosDbRepository<>), ServiceLifetime.Scoped));builder.Services.AddAzureADB2CAuthentication<User>(builder.Configuration);
-
-builder.Services.AddScoped<KitRepository>();
-builder.Services.AddScoped<UserRepository>();
-builder.Services.AddOutputCache();
-builder.Services.AddBlazoredLocalStorage();
-
-var app = builder.Build();
-//app.UseBlossom();
-app.UseAntiforgery();
-app.UseAuthentication();
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    services.AddCosmos<KodekitContext>(config["ConnectionStrings:CosmosDb"]!, "kodekit", ServiceLifetime.Scoped);
+    services.AddPasswordless<User>(config);
+    services.AddScoped<KitRepository>()
+            .AddScoped<UserRepository>();
+});
 
 app.Run();
