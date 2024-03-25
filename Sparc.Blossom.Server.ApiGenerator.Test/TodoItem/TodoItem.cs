19d33773b60233e4d74a75d6f34357a98f20ffae
Joseph Young <joseph@josephayoung.com>
2024-03-25 09:34:20 -0500

    Working incremental Api Generator with test console app

---
+++ Sparc.Blossom.Server.ApiGenerator.Test/TodoItem/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using Sparc.Blossom.Data;
+
+namespace Sparc.Blossom.Example.Single.TodoItem;
+
+public class TodoItem(string title, string description) : Entity<string>(Guid.NewGuid().ToString())
+{
+    public string? ListId { get; set; }
+    public string Title { get; set; } = title;
+    public string Description { get; set; } = description;
+    public bool IsDone { get; set; }
+
+    public void MarkDone()
+    {
+        IsDone = true;
+    }
+
+    public void MoveToNewList(string listId)
+    {
+        ListId = listId;
+    }
+}
---
+++ Sparc.Blossom.Server.ApiGenerator.Test/TodoItem/TodoItems.cs
@@ -0,0 +1,8 @@
+﻿using Sparc.Blossom.Data;
+
+namespace Sparc.Blossom.Example.Single.TodoItem;
+
+public partial class TodoItems
+{
+    public IEnumerable<TodoItem> Open(IRepository<TodoItem> items) => items.Query.Where(x => x.IsDone == false);
+}
