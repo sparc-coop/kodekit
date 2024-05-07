73193021bff92f13bcdd4bb010874ac5264baac6
Christine Antonio <christinejm.antonio@gmail.com>
2024-05-07 15:57:49 -0700

    reorganize pages, test home page for home page background and cursor

--- Kodekit.Web/wwwroot/cursor.js
+++ Kodekit.Web/wwwroot/cursor.js
@@ -10,6 +10,19 @@ var follow = document.createElement("div");
 follow.classList.add("follow");
 document.body.appendChild(follow);
 
+var start = document.createElement("div");
+start.classList.add("cursor-start");
+var startImg = document.createElement("img");
+startImg.setAttribute("src", "blobs/cursor-text.svg");
+startImg.classList.add("cursor-text");
+start.appendChild(startImg);
+document.body.appendChild(start);
+
+var shadow = document.createElement("img");
+shadow.classList.add("cursor-shadow");
+shadow.setAttribute("src", "blobs/blob-black.svg");
+document.body.appendChild(shadow);
+
 function move(obj, event) {
     obj.style = "";
     obj.style.top = event.clientY + "px";
@@ -23,8 +36,10 @@ if (cursor) {
     window.addEventListener("mousemove", function (event) {
         var e = event;
         var t = e.target;
-        var f = follow;
         var c = cursor;
+        var f = follow;
+        var st = start;
+        var cs = shadow;
 
         if (t == null || t.parentElement == null)
             return;
@@ -36,8 +51,20 @@ if (cursor) {
             f.style.left = t.offsetLeft + "px";
             f.style.width = t.clientWidth + "px";
             f.style.height = t.clientHeight + "px";
-
             f.classList.add("on-focus");
+
+            st.style.top = (t.offsetTop) + "px";
+            st.style.left = (t.offsetLeft) + "px";
+            st.style.width = t.clientWidth + "px";
+            st.style.height = t.clientHeight + "px";
+            st.classList.add("on-focus");
+
+            cs.style.top = t.offsetTop + "px";
+            cs.style.left = t.offsetLeft + "px";
+            cs.style.width = t.clientWidth + "px";
+            cs.style.height = t.clientHeight + "px";
+            cs.classList.add("on-focus");
+
         } if (t.tagName == "NAV" ||
             t.parentElement.tagName == "NAV" ||
             (t.parentElement.classList.contains("menu-left") && t.tagName == "H1") ||
@@ -54,11 +81,16 @@ if (cursor) {
             t.parentElement.classList.contains("overlay"))
         {
             f.style.display = "none";
+            st.style.display = "none";
             move(c, e);
         } else {
             move(c, e);
             move(f, e);
+            move(st, e);
+            move(cs, e);
             f.classList.remove("on-focus");
+            st.classList.remove("on-focus");
+            cs.classList.remove("on-focus");
         }
     });
 //    window.addEventListener("mouseover", function (event) {
