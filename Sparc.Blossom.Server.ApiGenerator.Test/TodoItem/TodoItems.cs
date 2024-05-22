7b7844d9db8d6b948233c162d1912ee67d8245e8
Joseph Young <joseph@josephayoung.com>
2024-05-22 10:01:12 -0500

    Update dependencies and set version to 8.0.0

--- Sparc.Blossom.Server.ApiGenerator.Test/TodoItem/TodoItems.cs
+++
@@ -1,8 +0,0 @@
-﻿using Ardalis.Specification;
-
-namespace Sparc.Blossom.Example.Single.TodoItem;
-
-public class Open : Specification<TodoItem>
-{
-    public Open() => Query.Where(x => x.IsDone == false);
-}
