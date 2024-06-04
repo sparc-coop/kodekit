8a7a71988ad22f8e5bd469697064dfa2a1ce9468
Joseph Young <joseph@josephayoung.com>
2024-06-04 09:22:59 -0500

    Change preview background color container to label Fix various null reference exceptions

--- Kodekit.Web/wwwroot/cursor.js
+++ Kodekit.Web/wwwroot/cursor.js
@@ -26,12 +26,14 @@ function move(event) {
 
     //if (t.tagName == "NAV" || t.classList.contains("sidebar") || t.tagName == "BUTTON" || t.tagName == "A" || t.parentElement.classList.contains(
     //    "menu-right") || t.classList.contains("page-info") || t.parentElement.classList.contains("page-info") || t.parentElement.classList.contains("page-info--container") || t.parentElement.classList.contains("info")) {
-    if (t.classList.contains("hide-cursor-follow") || t.tagName == "BUTTON" || t.tagName == "A") {
-        cursorFollow.style.opacity = "0";
-        cursorText.style.opacity = "0";
-    } else {
-        cursorFollow.style.opacity = "1";
-        cursorText.style.opacity = "1";
+    if (cursorFollow && cursorText) {
+        if (t.classList.contains("hide-cursor-follow") || t.tagName == "BUTTON" || t.tagName == "A") {
+            cursorFollow.style.opacity = "0";
+            cursorText.style.opacity = "0";
+        } else {
+            cursorFollow.style.opacity = "1";
+            cursorText.style.opacity = "1";
+        }
     }
 
     //e.preventDefault();
