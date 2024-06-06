d290258a9ede843b6f76b5cae268f9fcada3184c
Laura Ferraz <lauraferraz5@gmail.com>
2024-06-06 11:58:25 -0300

    Create get kit current revision endpoint

---
+++ Kodekit.Features/Kits/GetKitCurrentRevision.cs
@@ -0,0 +1,17 @@
+﻿namespace Kodekit.Features;
+
+public class GetKitCurrentRevision : PublicFeature<string, KitRevision>
+{
+    public GetKitCurrentRevision(KitRepository kits)
+    {
+        Kits = kits;
+    }
+
+    private readonly KitRepository Kits;
+
+    public override async Task<KitRevision> ExecuteAsync(string kitId)
+    {
+        var currentRevision = await Kits.GetCurrentRevisionAsync(kitId);
+        return currentRevision;
+    }
+}
