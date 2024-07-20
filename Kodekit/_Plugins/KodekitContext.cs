1011dbe0d200bd0cabd1cec373a79ecc4967db22
Joseph Young <joseph@josephayoung.com>
2024-07-20 14:12:44 -0500

    Refactor authentication to use cookies
    This commit represents a major overhaul in the application's authentication mechanism, transitioning from a device-based authentication approach to a simplified, cookie-based authentication system utilizing ASP.NET Core's built-in features. Key changes include the addition of build configurations for two projects, removal of dependencies on `Sparc.Blossom.Authentication` in `KodekitContext.cs`, and the introduction of `BlossomDefaultAuthenticator` and its middleware for handling user authentication and state management. The `User` entity configuration has been updated to use a partition key on `UserId`, and various classes and methods related to device-based authentication have been removed, indicating a shift towards a more standardized authentication process. Additionally, updates to service registration to support cookie authentication and the removal of custom claims principal providers further align the application with ASP.NET Core's authentication mechanisms.

--- Kodekit/_Plugins/KodekitContext.cs
+++ Kodekit/_Plugins/KodekitContext.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Sparc.Blossom.Authentication;
 
 namespace Kodekit;
 
-public partial class KodekitContext
-    (DbContextOptions<KodekitContext> options, BlossomClaimsPrincipalProvider auth) 
+public partial class KodekitContext(DbContextOptions<KodekitContext> options, IHttpContextAccessor auth) 
     : BlossomDbContext(options, auth)
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<User>().HasNoKey();
+        builder.Entity<User>().ToContainer("Users").HasPartitionKey(x => x.UserId);
 
         var kit = builder.Entity<Kit>().HasPartitionKey(x => x.KitId).HasQueryFilter(x => x.UserId == UserId);
         var revision = builder.Entity<KitRevision>().HasPartitionKey(x => x.KitId);
