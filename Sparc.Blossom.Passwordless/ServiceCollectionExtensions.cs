fff46355488976fc2b5a6ada0f1dc2db21df8111
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2024-08-20 14:51:32 -0300

    Working version of Passwordless plugin with Kodekit

--- Sparc.Blossom.Passwordless/ServiceCollectionExtensions.cs
+++ Sparc.Blossom.Passwordless/ServiceCollectionExtensions.cs
@@ -5,6 +5,8 @@ using Sparc.Blossom.Server.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Sparc.Blossom.Passwordless;
+using Passwordless.Net;
+using Microsoft.Extensions.Configuration;
 
 namespace Sparc.Blossom.Authentication;
 
@@ -13,20 +15,14 @@ public static class ServiceCollectionExtensions
     public static WebApplicationBuilder AddBlossomPasswordlessAuthentication<TUser>(this WebApplicationBuilder builder)
         where TUser : BlossomUser, new()
     {
+        var passwordlessSettings = builder.Configuration.GetRequiredSection("Passwordless");
+        builder.Services.Configure<PasswordlessOptions>(passwordlessSettings);
+        builder.Services.AddPasswordlessSdk(passwordlessSettings.Bind);
+
         builder.Services.AddScoped<AuthenticationStateProvider, BlossomPasswordlessAuthenticator<TUser>>()
             .AddScoped<BlossomPasswordlessAuthenticator<TUser>>()
             .AddScoped(typeof(IBlossomAuthenticator), typeof(BlossomPasswordlessAuthenticator<TUser>));
 
         return builder;
-    }
-
-    public static IApplicationBuilder UseBlossomPasswordlessAuthentication(this IApplicationBuilder app)
-    {
-        app.UseCookiePolicy(new() { MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Strict });
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.UseMiddleware<BlossomDefaultAuthenticatorMiddleware>();
-
-        return app;
     }
 }
